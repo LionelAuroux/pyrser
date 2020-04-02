6ddf6267687f384b9dfbc5099cb395ede4f272ea
Gitea <gitea@fake.local>
2020-04-02 22:05:29 +0200

    Cleaning for v1

--- dbg.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import pdb
-from tests.internal_ast import *
-import unittest
-
-unittest.main()
--- pyrser/directives/__init__.py
+++
@@ -1,7 +0,0 @@
-from pyrser.directives.ignore import Ignore
-from pyrser.directives.trace import Trace
-
-__all__ = [
-    'Ignore',
-    'Trace',
-]
